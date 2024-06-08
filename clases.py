33238fc5501c46585ba43fd731edadaf795ab796
manuel <man.gonzalez@duocuc.cl>
2024-06-08 12:47:47 -0400

    arreglo de lista

---
+++ clases.py
@@ -0,0 +1,7 @@
+#arreglos (listas)
+numero = [1,2,3,4,5]
+# imprimir 
+print (" lista de numero", numero)
+# imprimir numeros individuales 
+print ("lista de numeros",numero [2])
+print (" lista de numeros",numero[-1])
